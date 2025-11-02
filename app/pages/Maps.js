274c0d079240392c7dad0bc9663fefc656868df8
meyerowitz <meyerowitzrebeca@gmail.com>
2025-11-02 13:02:39 -0400

    .

--- app/pages/Maps.js
+++ app/pages/Maps.js
@@ -1,9 +1,12 @@
-import { AppleMaps, GoogleMaps } from 'expo-maps';
+
 import { Platform, Text } from 'react-native';
 
 export default function Maps() {
     return(
-        <GoogleMaps.View style={{ flex: 1 }} />
+        <>
+        
+        </>
     );
+ 
 
 }
